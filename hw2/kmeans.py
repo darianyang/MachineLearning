a074d248af3249626bc25d40f5c4ef03236244e4
Darian T. Yang <dty7@pitt.edu>
2022-10-02 15:41:26 -0400

    about to try implementing a loss function for kmeans

--- hw2/kmeans.py
+++ hw2/kmeans.py
@@ -4,6 +4,7 @@ Initially written 2022-10-02 by DTY.
 Inspired by: 
 https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
 https://towardsdatascience.com/create-your-own-k-means-clustering-algorithm-in-python-d7d4c9077670
+https://analyticsarora.com/k-means-for-beginners-how-to-build-from-scratch-in-python/
 """
 
 import numpy as np
@@ -22,6 +23,9 @@ class KMeans:
             The number of clusters to form as well as the number of centroids to generate.
         max_iter : int
             Maximum number of iterations of the k-means algorithm for a single run.
+        n_init : int, default=10 (TODO)
+            Number of times the k-means algorithm will be run with different centroid seeds. 
+            The final results will be the best output of n_init consecutive runs in terms of inertia.
         distance_metric : str (TODO)
             Distance calculations can be 'euclidean', 'jaccard', 'cosine', or 'manhattan'.
         """
@@ -31,20 +35,48 @@ class KMeans:
 
     def euclidean(self, point, data):
         """
-        Euclidean distance (sum of squares) between point & data.
+        Euclidean distance between point & data.
         Point has dimensions (m,), data has dimensions (n,m), and output will be of size (n,).
         """
         return np.sqrt(np.sum((point - data)**2, axis=1))
 
+    def _calc_inertia(self):
+        """
+        Calculate the inertia of a clustering run.
+        """
+        pass
+
     # TODO
-    def fit(self, X_train):
+    def _get_loss(self, centers, cluster_idx, points):
+        """
+        The loss function is the metric by which we evaluate the performance of our clustering algorithm. 
+        Our loss is simply the sum of the square distances between each point and its cluster centroid.
+
+        Args:
+            centers: KxD numpy array, where K is the number of clusters, and D is the dimension
+            cluster_idx: numpy array of length N, the cluster assignment for each point
+            points: NxD numpy array, the observations
+        Return:
+            loss: a single float number, which is the objective function of KMeans. 
+        """
+        dists = self.pairwise_dist(points, centers)
+        loss = 0.0
+        N, D = points.shape
+        for i in range(N):
+            loss = loss + np.square(dists[i][cluster_idx[i]])
+        
+        return loss
+
+    def _init_centers(self):
         """
         Initialize the centroids, using the "k-means++" method, where a random datapoint is selected as the first,
         then the rest are initialized w/ probabilities proportional to their distances to the first.
         """
+        # TODO: put this whole process in a loop for the n_init
         # Pick a random point from train data for first centroid
         self.centroids = [random.choice(X_train)]
 
+        # k-means++
         for _ in range(self.n_clusters-1):
             # Calculate distances from points to the centroids
             dists = np.sum([self.euclidean(centroid, X_train) for centroid in self.centroids], axis=0)
@@ -60,10 +92,13 @@ class KMeans:
         # min_, max_ = np.min(X_train, axis=0), np.max(X_train, axis=0)
         # self.centroids = [uniform(min_, max_) for _ in range(self.n_clusters)]
 
+    def fit(self, X_train):
+        # first initialize centers using k-means++
+        self._init_centers()
+
         # Iterate, adjusting centroids until converged or until passed max_iter
         iteration = 0
         prev_centroids = None
-
         while np.not_equal(self.centroids, prev_centroids).any() and iteration < self.max_iter:
             
             # Sort each datapoint, assigning to nearest centroid
@@ -79,7 +114,8 @@ class KMeans:
             self.centroids = [np.mean(cluster, axis=0) for cluster in sorted_points]
             
             for i, centroid in enumerate(self.centroids):
-                if np.isnan(centroid).any():  # Catch any np.nans, resulting from a centroid having no points
+                # Catch any np.nans, resulting from a centroid having no points
+                if np.isnan(centroid).any():
                     self.centroids[i] = prev_centroids[i]
             
             iteration += 1
