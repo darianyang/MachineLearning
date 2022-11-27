ebe8bac464e726d2695736ce34cf20a9abf2ba28
Darian T. Yang <dty7@pitt.edu>
2022-11-27 17:37:26 -0500

    coded and created initial ∆feat ml_input dataset

--- final_project/ml.py
+++ final_project/ml.py
@@ -4,9 +4,10 @@ TODO: Eventually, integrate into wedap.
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
 import h5py
+
+from tqdm.auto import tqdm
+import sys
 
 ### helper functions normally avail in wedap ###
 def get_parents(f, walker_tuple):
@@ -14,24 +15,16 @@ def get_parents(f, walker_tuple):
     parent = f[f"iterations/iter_{it:08d}"]["seg_index"]["parent_id"][wlk]
     return it-1, parent
 
-def trace_walker(walker_tuple):
+def trace_walker(f, walker_tuple):
     # Unroll the tuple into iteration/walker 
     it, wlk = walker_tuple
     # Initialize our path
     path = [(it,wlk)]
     # And trace it
     while it > 1: 
-        it, wlk = get_parents((it, wlk))
+        it, wlk = get_parents(f, (it, wlk))
         path.append((it,wlk))
     return np.array(sorted(path, key=lambda x: x[0]))
-
-def get_coords(path, data_name, data_index):
-    # Initialize a list for the pcoords
-    coords = []
-    # Loop over the path and get the pcoords for each walker
-    # for it, wlk in path:
-    #     coords.append(self._get_data_array(data_name, data_index, it)[wlk][::10])
-    return np.array(coords)
 
 def create_ml_input(h5="data/ctd_ub_1d_v00.h5", last_iter=137):
     """
@@ -48,6 +41,7 @@ def create_ml_input(h5="data/ctd_ub_1d_v00.h5", last_iter=137):
     last_iter : int
         The iteration to consider for data extraction.
         Using 137 based on the ctd_ub_1d_v00 dataset.
+    # TODO: option to skip certain auxdata features (e.g. secondary structure)
 
     Returns
     -------
@@ -63,17 +57,72 @@ def create_ml_input(h5="data/ctd_ub_1d_v00.h5", last_iter=137):
     # correctly choosen
     #print(segs_in_last)
 
+    # save tau value (columns of pcoord array)
+    tau = np.atleast_3d(np.array(f[f"iterations/iter_{last_iter:08d}/pcoord"])).shape[1]
+
     # n features per iteration (must be constant for all iterations)
     n_features = len(list(f[f"iterations/iter_{last_iter:08d}/auxdata"]))
     # and every pcoord tracked (n depth)
-    n_features += np.atleast_3d(np.array(f[f"iterations/iter_{last_iter:08d}/pcoord"])).shape[2]
+    n_pcoords = np.atleast_3d(np.array(f[f"iterations/iter_{last_iter:08d}/pcoord"])).shape[2]
+    n_features += n_pcoords
 
-    # initialize correctly shaped array
+    # get the names of each feature
+    feat_names = [f"pcoord_{dim}" for dim in range(n_pcoords)] + \
+                 list(f[f"iterations/iter_{last_iter:08d}/auxdata"])
+    #print(feat_names)
+
+    # initialize empty but correctly shaped array
     ml_input = np.zeros((segs_in_last, last_iter * n_features))
+    # as expected, it is 24 by (137i * (47 aux + 2 pcoord) features) = 6713 rows
     #print(ml_input.shape)
 
-    
+    # testing trace output
+    #print(trace_walker(f, (last_iter, 2))[0])
 
+    ### fill the empty array ###
+    # loop each segment index of the last_iter choosen
+    for seg_i in tqdm(range(segs_in_last), desc="Creating initial input array"):
+        # get trace path back to bstate
+        trace = trace_walker(f, (last_iter, seg_i))
+        
+        # take trace path and loop each (it, wlk) item (begins at i1)
+        for it, wlk in trace:
+            
+            # loop each feature name
+            for feat_i, feat in enumerate(feat_names):
+                
+                # need to account for pcoords before auxdata
+                if feat[:-2] == "pcoord":
+                    feat_name = "pcoord"
+                    feat_depth = int(feat[-1:])
+                else:
+                    feat_name = f"auxdata/{feat}"
+                    # TODO: eventually can account for multi depth dim auxdata
+                    feat_depth = 0
+
+                ### calc the ∆feat value of each feature of current (it, wlk) ###
+                # first grab the correctly indexed (it, wlk, feat) array from h5 file
+                it_wlk_feat_data = np.atleast_2d(f[f"iterations/iter_{it:08d}/{feat_name}"][wlk])
+                # properly shape the array for ndims of auxdata
+                if feat_name[:7] == "auxdata":
+                    # (if tau=11) goes from row to column (1, 11) to (11, 1); pcoord is (11, n)
+                    it_wlk_feat_data = it_wlk_feat_data.reshape(tau, 1)
+
+                # ∆feat = |last frame - first frame|
+                d_feat = np.absolute(it_wlk_feat_data[:, feat_depth][-1] - 
+                                     it_wlk_feat_data[:, feat_depth][0])
+
+                # assign values of ml_input array (row=seg_i, col=feat_i+((it-1)*n_features))
+                ml_input[seg_i, feat_i + ((it - 1) * n_features)] = d_feat
+
+    return ml_input
+            
+
+# eventually for optimized weight tracking, make dict of aux names
 
 if __name__ == "__main__":
-    create_ml_input()
+    # ml_input = create_ml_input()
+    # np.savetxt("we_ml_input.tsv", ml_input, delimiter="\t")
+    
+    ml_input = np.loadtxt("we_ml_input.tsv")
+    print(ml_input)
